26f6a79471caf2e341820715b0fccbfab5d53785
melanieeleon <melanieleon20@hotmail.com>
2024-12-03 10:02:26 -0500

    Se añadio la parte de recordatorios en el mainpage ademas, se arreglaron errores del la pagina index y de la mainpage, y se borro codigo que no estaba siendo utilizado.

---
+++ Login/mainpageScript.js
@@ -0,0 +1,73 @@
+
+// Info date
+const dateNumber = document.getElementById('dateNumber');
+const dateText = document.getElementById('dateText');
+const dateMonth = document.getElementById('dateMonth');
+const dateYear = document.getElementById('dateYear');
+
+// Tasks Container
+const tasksContainer = document.getElementById('tasksContainer');
+
+const setDate = () => {
+    const date = new Date();
+    dateNumber.textContent = date.toLocaleString('en', { day: 'numeric' });
+    dateText.textContent = date.toLocaleString('en', { weekday: 'long' });
+    dateMonth.textContent = date.toLocaleString('en', { month: 'short' });
+    dateYear.textContent = date.toLocaleString('en', { year: 'numeric' });
+};
+
+const addNewTask = (event) => {
+    event.preventDefault();
+    const { value } = event.target.taskText;
+    if (!value) return;
+
+    // Crear la tarea
+    const task = document.createElement('div');
+    task.classList.add('task', 'roundBorder');
+
+    // Crear el texto de la tarea
+    const taskText = document.createElement('span');
+    taskText.textContent = value;
+
+    // Crear el botón de eliminar
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = 'Delete';
+    deleteButton.classList.add('deleteButton');
+    deleteButton.addEventListener('click', (event) => {
+        event.stopPropagation(); // Evita activar `changeTaskState`
+        deleteTask(task); // Pasa la tarea como argumento
+    });
+
+    // Agregar eventos y estructura
+    task.addEventListener('click', changeTaskState);
+    task.appendChild(taskText);
+    task.appendChild(deleteButton);
+
+    tasksContainer.prepend(task); // Añadir al contenedor
+    event.target.reset(); // Limpiar el formulario
+};
+
+const changeTaskState = (event) => {
+    event.target.classList.toggle('done');
+};
+
+const order = () => {
+    const done = [];
+    const toDo = [];
+    tasksContainer.childNodes.forEach((el) => {
+        el.classList.contains('done') ? done.push(el) : toDo.push(el);
+    });
+    return [...toDo, ...done];
+};
+
+const renderOrderedTasks = () => {
+    const orderedTasks = order();
+    tasksContainer.innerHTML = ""; // Limpia el contenedor
+    orderedTasks.forEach((el) => tasksContainer.appendChild(el)); // Renderiza en orden
+};
+
+const deleteTask = (task) => {
+    tasksContainer.removeChild(task);
+};
+
+setDate();
